36278dda30b0c988c455996221d90fe90068731f
huntmori04x22 <huntmori04x22@gmail.com>
2021-01-31 17:40:28 +0000

    add UsersSevice mehotds

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -21,4 +21,28 @@ export class UsersService
     {
         return compare(password, user.password);
     }
+
+    public async createdUserFromRequest(request: RegisterRequest): Promise<User>
+    {
+        const { username, password } = request;
+
+        const existingFromUsername = await this.findForUsername(request.username);
+
+        if(existingFromUsername)
+        {
+            throw new UnprocessableEntityException('Username already in use');
+        }
+
+        return this.users.create(username, password);
+    }
+
+    public async findForId(id: number): Promise<User | null>
+    {
+        return this.users.findForId(id);
+    }
+
+    public async findForUsername (username: string): Promise<User | null>
+    {
+        return this.findForUsername(username);
+    }
 }

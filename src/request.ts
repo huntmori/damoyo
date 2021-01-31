09db293c50aa4fd1559ab428117d057b7b2ca757
huntmori04x22 <huntmori04x22@gmail.com>
2021-01-31 16:48:28 +0000

    add request classes

--- src/request.ts
+++ src/request.ts
@@ -1,1 +1,26 @@
 // src/request.ts
+import { IsNotEmpty, MinLength } from 'class-validator';
+
+export class LoginRequest
+{
+    @IsNotEmpty({message:'A username is required'})
+    readonly username: string;
+
+    @IsNotEmpty({message:'A password is required to login'})
+    readonly password: string;
+}
+
+export class RegisterRequest
+{
+    @IsNotEmpty({message:'An username is required'})
+    readonly username: string;
+
+    @IsNotEmpty({message:'A password is required'})
+    readonly password: string;
+}
+
+export class RefreshRequest
+{
+    @IsNotEmpty({message:'The refresh token is required'})
+    readonly refresh_token: string;
+}

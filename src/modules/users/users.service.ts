df6595200bc0bf07aef73e235917683d8b5703f5
huntmori04x22 <huntmori04x22@gmail.com>
2021-02-02 07:22:29 +0000

    commit내역 test

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -21,7 +21,7 @@ export class UsersService
     {
         return compare(password, user.password);
     }
-
+    //commit test
     public async createdUserFromRequest(request: RegisterRequest): Promise<User>
     {
         const { username, password } = request;

2693657c6ffa4c5722f504ae3f2ed465ae45abd4
huntmori <huntmori04x22@gmail.com>
2021-02-04 15:24:33 +0000

    add login method in authentication controller

--- src/modules/authentication/authentication.controller.ts
+++ src/modules/authentication/authentication.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { RegisterRequest } from '../../request';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { LoginRequest, RegisterRequest } from '../../request';
 import { User } from '../../models/user.model';
 import { UsersService } from '../users/users.service';
 import { TokenService } from './tokens.service';
+import { access } from 'fs';
 
 export interface AuthenticationPayload{
     user: User;
@@ -28,11 +29,53 @@ export class AuthenticationController
     @Post('/register')
     public async register(@Body() body: RegisterRequest)
     {
+        const user = await this.users.createdUserFromRequest(body);
 
+        const token = await this.tokens.generateAcessToken(user);
+
+        const refresh = await this.tokens.generateRefreshToken(user, 60*60*24*30);
+
+        const payload = this.buildResponsePayload(user, token, refresh);
+
+        return {
+            status: 'success',
+            data: payload
+        }
+    }
+
+    @Post('/login')
+    public async login(@Body() body: LoginRequest)
+    {
+        const { username, password } = body;
+
+        const user = await this.users.findForUsername(username);
+        const valid = user ? await this.users.validateCredentials(user, password) : false;
+
+        if (!valid)
+        {
+            throw new UnauthorizedException('The login is invalid');
+        }
+
+        const token = await this.tokens.generateAcessToken(user);
+        const refresh = await this.tokens.generateRefreshToken(user, 60*60*24*30);
+
+        const payload = this.buildResponsePayload(user, token, refresh);
+
+        return {
+            status: 'success',
+            data: payload,
+        };
     }
 
     private buildResponsePayload(user: User, accessToken: string, refreshToken?: string): AuthenticationPayload
     {
-        return null;
+        return {
+            user: user,
+            payload: {
+                type: 'bearer',
+                token: accessToken,
+                ...(refreshToken ? { refresh_token : refreshToken}: {}),
+            }
+        };
     }
 }
